4c36fd4456382faf87e64d8340171bb4b0cf9ca1
Kanna Kalyan <kannakalyan05@gmail.com>
2023-09-22 17:02:16 +0530

    Last updated on 22/09/2023 17:02:16

---
+++ bspwm_install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# List of packages to install
+packages=(
+    bspwm
+    xorg
+    lightdm
+    lightdm-gtk-greeter
+    lightdm-gtk-greeter-settings
+    sxhkd
+    polybar
+    picom
+    ttf-indic-otf
+    ttf-jetbrains-mono-nerd
+    htop
+    dmenu
+    pcmanfm
+)
+
+# Install the packages using pacman
+sudo pacman -S "${packages[@]}"
+
+# Create configuration directories
+mkdir -p ~/.config/{bspwm,sxhkd}
+
+# Copy bspwm and sxhkd config files
+install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
+install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
+
+# Append additional lines to bspwmrc
+echo "nitrogen --restore &" >> ~/.config/bspwm/bspwmrc
+echo "setxkbmap us &" >> ~/.config/bspwm/bspwmrc
+echo "polybar &" >> ~/.config/bspwm/bspwmrc
+echo "picom -f &" >> ~/.config/bspwm/bspwmrc
+
+# Enable the LightDM service
+sudo systemctl enable lightdm
+
