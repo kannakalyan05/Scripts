209414260d3c4ed45fbbc230402fe00cd428e16f
KalyanKanna <73469968+kannakalyan05@users.noreply.github.com>
2023-09-22 17:14:45 +0530

    Update bspwm_install.sh

--- bspwm_install.sh
+++ bspwm_install.sh
@@ -21,20 +21,3 @@ packages=(
 
 # Install the packages using pacman
 sudo pacman -S "${packages[@]}"
-
-# Create configuration directories
-mkdir -p ~/.config/{bspwm,sxhkd}
-
-# Copy bspwm and sxhkd config files
-install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
-install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
-
-# Append additional lines to bspwmrc
-echo "nitrogen --restore &" >> ~/.config/bspwm/bspwmrc
-echo "setxkbmap us &" >> ~/.config/bspwm/bspwmrc
-echo "polybar &" >> ~/.config/bspwm/bspwmrc
-echo "picom -f &" >> ~/.config/bspwm/bspwmrc
-
-# Enable the LightDM service
-sudo systemctl enable lightdm
-
