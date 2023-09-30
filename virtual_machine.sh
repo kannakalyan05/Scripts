d9af06d454e0abfc3559de50e93577dd843c4013
Kanna Kalyan <kannakalyan05@gmail.com>
2023-09-30 13:11:43 +0530

    Last updated on 30/09/2023 13:11:43

--- virtual_machine.sh
+++ virtual_machine.sh
@@ -11,17 +11,17 @@ pacman -Syy
 pacman -S virt-manager virt-viewer qemu bridge-utils libguestfs
 
 # Edit the libvirtd.conf file
-config_file="/etc/libvirt/libvirtd.conf"
-if [ -f "$config_file" ]; then
+#config_file="/etc/libvirt/libvirtd.conf"
+#if [ -f "$config_file" ]; then
   # Uncomment the 'unix_sock_group' line and change 'unix_sock_ro_perms' value
-  sed -i '/^#unix_sock_group/s/^#//' "$config_file"
-  sed -i 's/#unix_sock_ro_perms = "0770"/unix_sock_ro_perms = "0770"/' "$config_file"
+#  sed -i '/^#unix_sock_group/s/^#//' "$config_file"
+ # sed -i 's/#unix_sock_ro_perms = "0770"/unix_sock_ro_perms = "0770"/' "$config_file"
 
-  echo "Configuration changes in $config_file applied."
-else
-  echo "$config_file does not exist."
-  exit 1
-fi
+  #echo "Configuration changes in $config_file applied."
+#else
+ # echo "$config_file does not exist."
+ # exit 1
+#fi
 
 # Add the current user to the libvirt group
 gpasswd -a $USER libvirt
