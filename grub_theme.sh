0f53d8e0b0be54f51d6a54192fd0028bf70e3bbb
kanna kalyan <kannakalyan05@gmail.com>
2024-09-13 14:15:25 +0530

    Last updated on 13/09/2024 14:15:25

--- grub_theme.sh
+++ grub_theme.sh
@@ -5,10 +5,10 @@ install_grub_theme() {
   # Ask the user if they want to install the Grub theme
   read -rep $'[\e[1;33mACTION\e[0m] - Would you like to install the Grub theme? (y,n) ' INST
   if [[ $INST == "Y" || $INST == "y" ]]; then
-    sudo rm -rf /boot/grub/themes/starfield
-    sudo mkdir /boot/grub/themes/CyberEXS
-    sudo git clone https://github.com/HenriqueLopes42/themeGrub.CyberEXS.git /boot/grub/themes/CyberEXS
-    echo 'GRUB_THEME="/boot/grub/themes/CyberEXS/theme.txt"' | sudo tee -a /etc/default/grub
+    sudo rm -rf /boot/efi/grub/themes/starfield
+    sudo mkdir /boot/efi/grub/themes/CyberEXS
+    sudo git clone https://github.com/HenriqueLopes42/themeGrub.CyberEXS.git /boot/efi/grub/themes/CyberEXS
+    echo 'GRUB_THEME="/boot/efi/grub/themes/CyberEXS/theme.txt"' | sudo tee -a /etc/default/grub
     reboot_option='menuentry "Reboot" {
         reboot
     }'
@@ -17,8 +17,8 @@ install_grub_theme() {
     }'
     echo "$reboot_option" | sudo tee -a /etc/grub.d/40_custom
     echo "$shutdown_option" | sudo tee -a /etc/grub.d/40_custom
-    if [ -f /boot/grub/grub.cfg ]; then
-        sudo grub-mkconfig -o /boot/grub/grub.cfg
+    if [ -f /boot/efi/grub/grub.cfg ]; then
+        sudo grub-mkconfig -o /boot/efi/grub/grub.cfg
     else
         echo "grub.cfg not found. Please update it manually."
     fi
