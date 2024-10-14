0f926a812a7793530dadbe04955285156cbaeb22
KalyanKanna <73469968+kannakalyan05@users.noreply.github.com>
2024-10-14 12:35:49 +0530

    added sddm

--- Kde_install.sh
+++ Kde_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the specific KDE packages to install
-kde_packages=("ark" "dolphin" "egl-wayland" "konsole" "plasma-meta" "plasma-workspace" "xdg-utils")
+kde_packages=("ark" "dolphin" "egl-wayland" "konsole" "plasma-meta" "plasma-workspace" "xdg-utils" "sddm")
 
 # Install the packages
 sudo pacman -S --noconfirm "${kde_packages[@]}"
